c4b752cda4ab310735bd8e85b1182fb079680851
tackman <tackman@tackman.info>
2018-12-25 18:35:38 +0900

    cleaned old emacs.d

--- .emacs.d/elpa/s-1.11.0/s-autoloads.el
+++
@@ -1,15 +0,0 @@
-;;; s-autoloads.el --- automatically extracted autoloads
-;;
-;;; Code:
-(add-to-list 'load-path (directory-file-name (or (file-name-directory #$) (car load-path))))
-
-;;;### (autoloads nil nil ("s.el") (22975 6294 856683 748000))
-
-;;;***
-
-;; Local Variables:
-;; version-control: never
-;; no-byte-compile: t
-;; no-update-autoloads: t
-;; End:
-;;; s-autoloads.el ends here
